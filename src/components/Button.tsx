881b16055490df17131a79c05d68b1f949b2cfe2
omiddev88 <omid.heidari.info@gmail.com>
2025-01-31 19:59:57 +0330

    make button more cute

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,13 @@
-import React from "react";
-
 const Button = () => {
-  return <div className="bg-grey-100">Button</div>;
+  return (
+    <div className="bg-grey-100">
+      <div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Button
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Button;
