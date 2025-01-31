9468ee45da095f8fbfd755d5c1f34cb47a882f66
omiddev88 <omid.heidari.info@gmail.com>
2025-01-31 15:15:35 +0330

    change test name

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,7 +2,7 @@ import { describe, it } from "vitest";
 import { db } from "./mocks/db";
 
 describe("group", () => {
-  it("should", () => {
+  it("should create a product", () => {
     const product = db.product.create({ name: "Apple" });
     console.log(db.product.delete({ where: { id: { equals: product.id } } }));
   });
