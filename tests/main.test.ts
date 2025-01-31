9504f855684b6138ac3e3d7556eba3b2f6ff3ed8
omiddev88 <omid.heidari.info@gmail.com>
2025-01-31 15:35:20 +0330

    Initial commit for feature new-feature

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,7 +2,7 @@ import { describe, it } from "vitest";
 import { db } from "./mocks/db";
 
 describe("group", () => {
-  it("should create a product", () => {
+  it("we are on feature", () => {
     const product = db.product.create({ name: "Apple" });
     console.log(db.product.delete({ where: { id: { equals: product.id } } }));
   });
